1b79fb7c5b9da8ccd35bb8f9cc9d0e339b3f8a5b
vladislava8 <90659016+vladislava8@users.noreply.github.com>
2023-11-26 20:32:53 -0600

    Add files via upload

---
+++ Firebase init.swift
@@ -0,0 +1,8 @@
+//
+//  Firebase init.swift
+//  SD-Fit
+//
+//  Created by Vladislava Sicicorez on 9/24/23.
+//
+
+import Foundation
---
+++ ViewController2.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController2.swift
+//  SD-Fit
+//
+//  Created by Vladislava Sicicorez on 10/29/23.
+//
+
+import AVKit
+import AVFoundation
+import UIKit
+
+
+class ViewController2: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
